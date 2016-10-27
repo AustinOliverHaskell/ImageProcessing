765ac06bc262c12ca845116cb8de8c55f9b5bc4f
AustinOliverHaskell <ahaskell@carthage.edu>
2016-10-26 21:44:25 -0500

    Pixel Data

--- ImageProc/ImageProc/Source.cpp
+++ ImageProc/ImageProc/Source.cpp
@@ -1,6 +1,16 @@
 #include "CImg.h"
 using namespace cimg_library;
 
+void shiftColor(CImg <unsigned char> &image, unsigned int shift, unsigned int tolerance,enum Colors color);
+void grabChannel(const CImg <unsigned char> &image, enum Colors color);
+
+enum Colors
+{
+	RED = 0,
+	GREEN,
+	BLUE
+};
+
 int main() {
 
 	// Open image
@@ -11,26 +21,56 @@ int main() {
 	int height = image.height();
 	int size = image.size();
 
-	// Image functions (Basic)
-	// image.blur(2.5);
-	// Blur on axis
-	// image.blur(0, 1); 
-	// image.noise(12);
-	
-	
+	grabChannel(image, BLUE);
+
+
+	shiftColor(image, 10, 0, BLUE);
+
 	// Create two displays (Windows)
 	CImgDisplay main_disp(image, "Stenography");
 
 
 	// Check to make sure that the displays arnt closed, if so then our porgram will end
-	while (!main_disp.is_closed()) 
+	while (!main_disp.is_closed())
 	{
-		cimg_for(image, ptr, unsigned char)
-		{ 
-			*ptr = 0; 
-		}       
 	}
+
 
 	// End
 	return 0;
 }
+
+void shiftColor(CImg <unsigned char> &image, unsigned int shift, unsigned int tolerance,enum Colors color)
+{
+	for (int y = 0; y < image.height(); y++)
+	{
+		for (int x = 0; x < image.width(); x++)
+		{
+			if (image(x, y, 0, color) > tolerance)
+			{
+				image(x, y, 0, color) += shift;
+			}
+		}
+	}
+}
+
+void grabChannel(const CImg <unsigned char> &image, enum Colors color)
+{
+	// Will seperate the color into a new image of same dimentions
+	CImg <unsigned char> retVal (image.width(), image.height(), image.depth(), image.spectrum());
+
+	for (int y = 0; y < image.height(); y++)
+	{
+		for (int x = 0; x < image.width(); x++)
+		{
+			retVal(x, y, 0, color) = image(x, y, 0, color);
+		}
+	}
+
+	//retVal.fill(100);
+
+	retVal.normalize(0, 255);
+	retVal.save("channel.jpg");
+
+}
+
